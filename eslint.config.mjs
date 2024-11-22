8eb4d587784f9a743405b996861995d59b679e56
Vlad <vlasisla1988@gmail.com>
2024-11-23 01:38:54 +0200

    feat: Add eslint settings

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import globals from 'globals';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+export default [
+  {
+    languageOptions: {
+      globals: globals.browser
+    }
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    // rules: {
+    //   "no-console": ['error', { allow: ['warn', 'error', 'info'] }],
+    // }
+  }, {
+    ignores: ['node_modules', 'dist']
+  }
+];
